b5e0c904a4e12bcd039cc9c93e0f2364c656b9fd
Vladislav Tsigel'nikov <tsigelnikovvlad@gmail.com>
2015-06-19 03:31:23 +0600

    калькулятор

--- js/KitSend.js
+++ js/KitSend.js
@@ -27,8 +27,10 @@ $(document).ready(function(){
 		if( $(this).find("input[name=phone]").length ){
 			$(this).find("input[name=phone]").mask(tePhone,{placeholder:"_"});
 		}
+		
 	});
 
+	
 	function whenScroll(){
 		var scroll = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
 		if( customHandlers["onScroll"] ){
@@ -109,23 +111,88 @@ $(document).ready(function(){
 			  	url: $(this).attr("action"),
 			  	data:  $this.serialize(),
 				success: function(msg){
-					if( msg == "1" ){
-						$form = $($this.attr("data-block"));
-					}else{
-						$form = $("#b-popup-error");
-					}
+					if(msg=='1' || msg=='0') {
+						if( msg == "1" ){
+							$form = $($this.attr("data-block"));
+						}else{
+							$form = $("#b-popup-error");
+						}
 
-					if( $this.attr("data-afterAjax") && customHandlers[$this.attr("data-afterAjax")] ){
-						customHandlers[$this.attr("data-afterAjax")]($this);
-					}
+						if( $this.attr("data-afterAjax") && customHandlers[$this.attr("data-afterAjax")] ){
+							customHandlers[$this.attr("data-afterAjax")]($this);
+						}
 
-					$this.find("input[type=text],textarea").val("");
-					$.fancybox.open({
-						content : $form,
-						padding : 0
-					});	
+						$this.find("input[type=text],textarea").val("");
+						$.fancybox.open({
+							content : $form,
+							padding : 0
+						});
+					} else {
+						// $.parseJSON(msg);
+						console.log(msg);
+					}	
 				}
 			});
+  		}
+  		return false;
+  	});
+	
+	$('#calc').find("input[type=text]").mask('99?9',{placeholder:" "});
+	$("#calc").submit(function(){
+  		if( $(this).valid() ){
+  			if($("input[name=sex]:checked").val()=='male') {
+				var HB = (66.47 + 13.75*parseInt($("input[name=weight]").val())+5*parseInt($("input[name=growth]").val())-6.74*parseInt($("input[name=age]").val()))*parseFloat($("select[name=lifestyle] option:selected").val());
+				var MG = (9.99*parseInt($("input[name=weight]").val())+6.25*parseInt($("input[name=growth]").val())-4.92*parseInt($("input[name=age]").val())+5)*parseFloat($("select[name=lifestyle] option:selected").val());
+			} else if($("input[name=sex]:checked").val()=='female') {
+				var HB = (655.1 + 9.6*parseInt($("input[name=weight]").val())+1.85*parseInt($("input[name=growth]").val())-4.68*parseInt($("input[name=age]").val()))*parseFloat($("select[name=lifestyle] option:selected").val());
+				var MG = (9.99*parseInt($("input[name=weight]").val())+6.25*parseInt($("input[name=growth]").val())-4.92*parseInt($("input[name=age]").val())-161)*parseFloat($("select[name=lifestyle] option:selected").val());
+			} 
+			HB = Math.round(HB);
+			MG = Math.round(MG);
+			if($("input[name=goal]:checked").val()=='decrease') {
+				var MG_min = Math.round(0.8*MG);
+				var MG_max = Math.round(0.9*MG);
+
+				var protein_min = Math.round(MG_min*0.25/4);
+				var carbo_min = Math.round(MG_min*0.4/4);
+				var fat_min = Math.round(MG_min*0.25/9);
+
+				var protein_max = Math.round(MG_max*0.35/4);
+				var carbo_max = Math.round(MG_max*0.50/4);
+				var fat_max = Math.round(MG_max*0.3/9);
+			}
+			if($("input[name=goal]:checked").val()=='normal') {
+				var MG_min = Math.round(0.95*MG);
+				var MG_max = Math.round(1.05*MG);
+
+				var protein_min = Math.round(MG_min*0.25/4);
+				var carbo_min = Math.round(MG_min*0.45/4);
+				var fat_min = Math.round(MG_min*0.25/9);
+
+				var protein_max = Math.round(MG_max*0.35/4);
+				var carbo_max = Math.round(MG_max*0.55/4);
+				var fat_max = Math.round(MG_max*0.3/9);
+			}
+			if($("input[name=goal]:checked").val()=='increase') {
+				var MG_min = Math.round(1.1*MG);
+				var MG_max = Math.round(1.2*MG);
+
+				var protein_min = Math.round(MG_min*0.3/4);
+				var carbo_min = Math.round(MG_min*0.45/4);
+				var fat_min = Math.round(MG_min*0.25/9);
+
+				var protein_max = Math.round(MG_max*0.35/4);
+				var carbo_max = Math.round(MG_max*0.55/4);
+				var fat_max = Math.round(MG_max*0.3/9);
+			}
+
+			$("#HB").text(HB);
+			$("#MG").text(MG);
+			$("#cal").text(MG_min+"-"+MG_max);
+			$("#protein").text(protein_min+"-"+protein_max+" гр.");
+			$("#carbo").text(carbo_min+"-"+carbo_max+" гр.");
+			$("#fat").text(fat_min+"-"+fat_max+" гр.");
+			$("#water").text((0.94*parseInt($("input[name=weight]").val())/30).toFixed(1)+" л.");
   		}
   		return false;
   	});
